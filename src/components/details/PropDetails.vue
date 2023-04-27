ffb06c6d2cbc318d024c552bb36281c34447ecca
Italo Schramm <italoschramm@hotmail.com>
2023-04-26 21:47:46 -0300

    Adjust tag description

--- src/components/details/PropDetails.vue
+++ src/components/details/PropDetails.vue
@@ -39,7 +39,7 @@ export default {
           title: 'PicPlace',
           meta: [
             {
-              property: 'og-description',
+              property: 'og:description',
               content: props.property.description
             },
             {
